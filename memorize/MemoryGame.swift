7377973dca9f2a4d5497d9bc7dd29e3cf1889581
Tiago Martinho <t.martinho@icloud.com>
2020-05-23 11:42:42 +0200

    Shuffle cards

--- memorize/MemoryGame.swift
+++ memorize/MemoryGame.swift
@@ -15,11 +15,12 @@ struct MemoryGame<CardContent> {
             cards.append(Card(id: pairIndex * 2, content: content))
             cards.append(Card(id: pairIndex * 2 + 1, content: content))
         }
+        cards.shuffle()
     }
     
     struct Card: Identifiable {
         var id: Int
-        var isFaceUp: Bool = false
+        var isFaceUp: Bool = true
         var isMatched: Bool = false
         var content: CardContent
     }
