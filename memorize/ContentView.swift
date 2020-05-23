bcdcecabaccea464957d9abb6f758f02b4af41d4
Tiago Martinho <t.martinho@icloud.com>
2020-05-23 11:00:33 +0200

    Initial Commit

---
+++ memorize/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  memorize
+//
+//  Created by Tiago Martinho on 23/05/2020.
+//  Copyright © 2020 Tiago Martinho. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
