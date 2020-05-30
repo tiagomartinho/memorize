73893572f92381f80493425283c62ffad0d2fd86
Tiago Martinho <t.martinho@icloud.com>
2020-05-30 13:39:01 +0200

    Add EmojiMemoryGameTheme

---
+++ memorize/AllEmojiMemoryGameThemes.swift
@@ -0,0 +1,5 @@
+extension EmojiMemoryGameTheme {
+    static var all: [EmojiMemoryGameTheme] = [EmojiMemoryGameTheme(name: "Halloween",
+                                                                   emojis: HalloweenEmojis.value, numberOfCards: 6,
+                                                                   color: .orange)]
+}
---
+++ memorize/EmojiMemoryGameTheme.swift
@@ -0,0 +1,8 @@
+import SwiftUI
+
+struct EmojiMemoryGameTheme {
+    let name: String
+    let emojis: Set<String>
+    let numberOfCards: Int
+    let color: UIColor
+}
